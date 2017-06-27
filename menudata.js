b5face359914d898a8832c52f79eaf5ec8dd37d6
Chris Bridge <chrisbridge44@googlemail.com>
2017-06-27 17:52:48 +0100

    Update docs

---
+++ menudata.js
@@ -0,0 +1,19 @@
+var menudata={children:[
+{text:"Main Page",url:"index.html"},
+{text:"Classes",url:"annotated.html",children:[
+{text:"Class List",url:"annotated.html"},
+{text:"Class Index",url:"classes.html"},
+{text:"Class Members",url:"functions.html",children:[
+{text:"All",url:"functions.html",children:[
+{text:"b",url:"functions.html#index_b"},
+{text:"c",url:"functions.html#index_c"},
+{text:"f",url:"functions.html#index_f"},
+{text:"g",url:"functions.html#index_g"},
+{text:"i",url:"functions.html#index_i"},
+{text:"r",url:"functions.html#index_r"},
+{text:"s",url:"functions.html#index_s"}]},
+{text:"Functions",url:"functions_func.html"},
+{text:"Enumerations",url:"functions_enum.html"},
+{text:"Enumerator",url:"functions_eval.html"}]}]},
+{text:"Files",url:"files.html",children:[
+{text:"File List",url:"files.html"}]}]}
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,14 +1,16 @@
 var searchData=
 [
-  ['getbasisinfo',['getBasisInfo',['../class_r_i_features_1_1_r_i_feat_extractor.html#ad88fc6e5ab6b401a2d85bb5772d330f2',1,'RIFeatures::RIFeatExtractor']]],
+  ['getbasisinfo',['getBasisInfo',['../class_r_i_features_1_1_r_i_feat_extractor.html#a1f11115cc7aaf70fbff288aba69a3695',1,'RIFeatures::RIFeatExtractor']]],
   ['getderivedfeature',['getDerivedFeature',['../class_r_i_features_1_1_r_i_feat_extractor.html#a6e1bc4d502b65c7bab5dcc7855d9c14e',1,'RIFeatures::RIFeatExtractor']]],
-  ['getfeatsusinglowj',['getFeatsUsingLowJ',['../class_r_i_features_1_1_r_i_feat_extractor.html#af47f4e9968b4d4e6a3e91777783d9b51',1,'RIFeatures::RIFeatExtractor']]],
-  ['getfeatswithgivenr',['getFeatsWithGivenR',['../class_r_i_features_1_1_r_i_feat_extractor.html#a93ebdbe5656aa961d655d7a23b536f04',1,'RIFeatures::RIFeatExtractor']]],
-  ['getfrequencybasiscopy',['getFrequencyBasisCopy',['../class_r_i_features_1_1_r_i_feat_extractor.html#ab49dc5a9e35e837087ca7728b6d7c3a5',1,'RIFeatures::RIFeatExtractor']]],
-  ['getmaxspatbasishalfsize',['getMaxSpatBasisHalfsize',['../class_r_i_features_1_1_r_i_feat_extractor.html#a8ea2d029f2244f18355cc8a85a3cf0c4',1,'RIFeatures::RIFeatExtractor']]],
-  ['getnumbases',['getNumBases',['../class_r_i_features_1_1_r_i_feat_extractor.html#a02e239f271a760c2db6bf2dddb62d66e',1,'RIFeatures::RIFeatExtractor']]],
-  ['getnumderivedfeats',['getNumDerivedFeats',['../class_r_i_features_1_1_r_i_feat_extractor.html#a4a9ff09635b710ca501a4d4f29e87ee8',1,'RIFeatures::RIFeatExtractor']]],
-  ['getnumrawfeats',['getNumRawFeats',['../class_r_i_features_1_1_r_i_feat_extractor.html#ae43ddb468a2d83bd2d800a55ccef2b58',1,'RIFeatures::RIFeatExtractor']]],
+  ['getderivedfeaturevector',['getDerivedFeatureVector',['../class_r_i_features_1_1_r_i_feat_extractor.html#a59868a6f9574817edbfc9f9c7a62fdd7',1,'RIFeatures::RIFeatExtractor']]],
+  ['getfeatsusinglowj',['getFeatsUsingLowJ',['../class_r_i_features_1_1_r_i_feat_extractor.html#a33e8a11672f8ff87a6224bfd44fb6d39',1,'RIFeatures::RIFeatExtractor']]],
+  ['getfeatswithgivenr',['getFeatsWithGivenR',['../class_r_i_features_1_1_r_i_feat_extractor.html#a5138b75562d7d33bef2f50a2d4d3b5d8',1,'RIFeatures::RIFeatExtractor']]],
+  ['getfrequencybasiscopy',['getFrequencyBasisCopy',['../class_r_i_features_1_1_r_i_feat_extractor.html#a3fd68f643fe0da3358e1fcba70a1c180',1,'RIFeatures::RIFeatExtractor']]],
+  ['getmaxspatbasishalfsize',['getMaxSpatBasisHalfsize',['../class_r_i_features_1_1_r_i_feat_extractor.html#af0775c8aac6d11548e481802a8b96617',1,'RIFeatures::RIFeatExtractor']]],
+  ['getnumbases',['getNumBases',['../class_r_i_features_1_1_r_i_feat_extractor.html#a9c30992e31d54d1c7f3fa7d99f01a7a1',1,'RIFeatures::RIFeatExtractor']]],
+  ['getnumderivedfeats',['getNumDerivedFeats',['../class_r_i_features_1_1_r_i_feat_extractor.html#a085f643773aed8db95a84e64b906139b',1,'RIFeatures::RIFeatExtractor']]],
+  ['getnumrawfeats',['getNumRawFeats',['../class_r_i_features_1_1_r_i_feat_extractor.html#a9337a05a8d826fff396d2eb88ca8b64d',1,'RIFeatures::RIFeatExtractor']]],
   ['getrawfeaturearg',['getRawFeatureArg',['../class_r_i_features_1_1_r_i_feat_extractor.html#a5bedc958816f76f8453f8841c820aaac',1,'RIFeatures::RIFeatExtractor']]],
-  ['getspatialbasiscopy',['getSpatialBasisCopy',['../class_r_i_features_1_1_r_i_feat_extractor.html#a42da9d253362a4fe751fa9f3505429c9',1,'RIFeatures::RIFeatExtractor']]]
+  ['getrawfeaturevector',['getRawFeatureVector',['../class_r_i_features_1_1_r_i_feat_extractor.html#a0160c2ddac865364422c67bc3c6a671b',1,'RIFeatures::RIFeatExtractor']]],
+  ['getspatialbasiscopy',['getSpatialBasisCopy',['../class_r_i_features_1_1_r_i_feat_extractor.html#a0cfcb13e25a3aa0514abd7c51013de20',1,'RIFeatures::RIFeatExtractor']]]
 ];
